ef91f2955fd9e3ade378a7c73acd721ea94bb590
Gunkar Singh <gunkar.singh@neofinancial.com>
2022-02-26 17:17:33 -0700

    Implement schema and resolver

---
+++ server.js
@@ -0,0 +1,20 @@
+const { gql, ApolloServer } = require("apollo-server")
+
+// interface
+const schemaDef = gql`
+    type Query {
+        message: String
+    }
+`
+
+// resolves the value of the greeting field (implementation)
+const resolvers = {
+    Query: {
+        message: () => "Hello Graphql world!"
+    }
+}
+
+const server = new ApolloServer({ typeDefs: schemaDef, resolvers: resolvers })
+server
+    .listen({ port: 9000 })
+    .then(({ port }) => console.log(`Server running on ${port}`))
