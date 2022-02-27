97c83cc2f8ce2b238963cbb950369dde0698af7d
Gunkar Singh <gunkar.singh@neofinancial.com>
2022-02-26 19:25:25 -0700

    Remove unneeded routes

--- server.js
+++ server.js
@@ -18,17 +18,6 @@ app.use(
     })
 )
 
-app.post("/login", (req, res) => {
-    const { email, password } = req.body
-    const user = db.users.list().find(user => user.email === email)
-    if (!(user && user.password === password)) {
-        res.sendStatus(401)
-        return
-    }
-    const token = jwt.sign({ sub: user.id }, jwtSecret)
-    res.send({ token })
-})
-
 app.get("/info", (req, res) => {
     res.send({ data: "send some information" })
 })
